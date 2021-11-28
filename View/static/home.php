e174e58d28bc9b2d7ed4386c95647bc269514f66
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2021-11-28 11:29:58 +0100

    Lien home vers le market

--- View/static/home.php
+++ View/static/home.php
@@ -12,7 +12,7 @@ require_once File::build_path(array("Controller", "routeur.php"));
             <div>
                 <p class="title">The NFT world at a few clicks</p>
                 <p class="title">Find rare digital art and collect NFTs</p>
-                <div class="button">Explore</div>
+                <div class="button"><a href="index.php?action=marketPlace&controller=Utilisateur">Explore</a></div>
             </div>
             <canvas>
                 ANIMATION THREE JS
@@ -44,57 +44,27 @@ require_once File::build_path(array("Controller", "routeur.php"));
     <h2>Popular products</h2>
 
     <section id="popular">
-
-
         <div class="card">
             <img src="ressources/NFT/drawing/face.jpeg" alt="" class="nft"/>
             <p>Drawing</p>
-            <div class="picture-profil">
+            <div class="market-detail">
                 <img src="ressources/profile.png" class="profil"">
                 <p>Jungle</p>
             </div>
             <div class="button">Add to cart</div>
         </div>
 
-        <div class="card">
-            <img src="ressources/NFT/drawing/girl_guy.jpg.webp" alt="" class="nft"/>
-            <p>Drawing</p>
-            <div class="picture-profil">
-                <img src="ressources/profile.png" class="profil"">
-                <p>Jungle</p>
-            </div>
-            <div class="button">Add to cart</div>
-        </div>
+        <?php
+            include "View/produit/aleatoire.php";
+        ?>
 
-        <div class="card">
-            <img src="ressources/NFT/drawing/road.jpeg" alt="" class="nft"/>
-            <p>Drawing</p>
-            <div class="picture-profil">
-                <img src="ressources/profile.png" class="profil"">
-                <p>Jungle</p>
-            </div>
-            <div class="button">Add to cart</div>
-        </div>
 
-        <div class="card">
-            <img src="ressources/NFT/drawing/squid.png.webp" alt="" class="nft"/>
-            <p>Drawing</p>
-            <div class="picture-profil">
-                <img src="ressources/profile.png" class="profil"">
-                <p>Jungle</p>
-            </div>
-            <div class="button">Add to cart</div>
-        </div>
 
-        <div class="card">
-            <img src="ressources/NFT/drawing/woman.jpeg" alt="" class="nft"/>
-            <p>Drawing</p>
-            <div class="picture-profil">
-                <img src="ressources/profile.png" class="profil"">
-                <p>Jungle</p>
-            </div>
-            <div class="button">Add to cart</div>
-        </div>
+
+
+
+
+
 
     </section>
 
