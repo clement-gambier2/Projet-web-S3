8ec10becba38a05d63d36e5df04ce5d186e7d237
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2021-12-02 08:10:08 +0100

    Affichage des données importantes

--- View/utilisateur/marketPlace.php
+++ View/utilisateur/marketPlace.php
@@ -16,10 +16,11 @@
             <img src="<?php echo $prod->get("lienImage") ?>" alt="" class="nft"/>
             <p><?php echo $prod->get("nomProduit") ?></p>
             <div class="market-detail">
-                <p><?php echo " IdProduit : " . " " . $idProduit ?></p>
-                <a href="index.php?action=read&controller=Produit&idProduit=<?php echo $idProduit ?>">Voir plus</a>
-                <?php echo $idCategorie ?>
+<!--                <p>--><?php //echo " IdProduit : " . " " . $idProduit ?><!--</p>-->
                 <?php echo $nomCategorie ?>
+                <a href="index.php?action=read&controller=Produit&idProduit=<?php echo $idProduit ?>">Voir plus</a>
+<!--                --><?php //echo $idCategorie ?>
+
             </div>
             <div class="button">
                 <input type="submit" name="idProduit" value ="Ajouter au panier"/>
