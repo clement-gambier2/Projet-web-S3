48054ee9b90477d3d198a59979cb7c9806e867a5
FreeZaxe <freezaxe4@gmail.com>
2021-12-07 22:26:16 +0100

    Sécurisation complète du site
    Un utilisateur non admin n'aura plus accès aux fonctionnalités admin

--- Controller/ControllerCommande.php
+++ Controller/ControllerCommande.php
@@ -28,44 +28,63 @@ class ControllerCommande {
     }
 
     public static function readAll() {
-        $tab_com = ModelCommande::selectAll();     //appel au modèle pour gerer la BD
-        $controller = static::$object;
-        $view = "list";
-        $pagetitle = "Commandes des users";
-        $list = File::build_path(array("View","view.php"));
-        require $list;
+        if ($_SESSION['admin'] == 1) {
+            $tab_com = ModelCommande::selectAll();     //appel au modèle pour gerer la BD
+            $controller = static::$object;
+            $view = "list";
+            $pagetitle = "Commandes des users";
+            $list = File::build_path(array("View","view.php"));
+            require $list;
+        } else {
+            $controller = "utilisateur";
+            $view = "connect";
+            $pagetitle = "Connectez vous pour accéder à cette fonctionnalité admin";
+            $list = File::build_path(array("View","view.php"));
+            require $list;
+        }
+        
     }
 
 
 
     public static function read(){
-        $idC = $_GET['idCommande'];
-        if(isset($_GET['idUtilisateur'])){
-            $idU = $_GET['idUtilisateur'];
+
+        if ($_SESSION['admin'] == 1) {
+            $idC = $_GET['idCommande'];
+            if(isset($_GET['idUtilisateur'])){
+                $idU = $_GET['idUtilisateur'];
+            }
+
+            $l = ModelProduit::getAllProduits($idC);
+            if(!$l){
+                $controller = static::$object;
+                $view = "error";
+                $pagetitle = "Erreur";
+                $erreur = File::build_path(array("View","view.php"));
+                require $erreur;
+            }
+            else{
+                $type = $_GET['type'];
+                $controller = static::$object;
+                $view = "detail";
+                $pagetitle = "Detail";
+                $detail = File::build_path(array("View","view.php"));
+                require $detail;
+            }
+        } else {
+            $controller = "utilisateur";
+            $view = "connect";
+            $pagetitle = "Connectez vous pour accéder à cette fonctionnalité admin";
+            $list = File::build_path(array("View","view.php"));
+            require $list;
         }
 
-        $l = ModelProduit::getAllProduits($idC);
-        if(!$l){
-            $controller = static::$object;
-            $view = "error";
-            $pagetitle = "Erreur";
-            $erreur = File::build_path(array("View","view.php"));
-            require $erreur;
-        }
-        else{
-            $type = $_GET['type'];
-            $controller = static::$object;
-            $view = "detail";
-            $pagetitle = "Detail";
-            $detail = File::build_path(array("View","view.php"));
-            require $detail;
-        }
     }
 
     public static function delete(){
         $idCommande = $_GET["idCommande"];
         $tab_com = ModelProduit::selectAll();
-        if (ModelCommande::delete($idCommande)) {
+        if ($_SESSION['admin'] == 1 && ModelCommande::delete($idCommande)) {
             if(isset($_GET['mesCommande'])){
                 $user = ModelUtilisateur::selectWithPseudo($_SESSION['login']);
                 $userId = $user->get('idUtilisateur');
@@ -99,13 +118,22 @@ class ControllerCommande {
     }
 
     public static function create(){
-        $tab_prod = ModelProduit::selectAll();
-        $tab_utilisateur = ModelUtilisateur::selectAll();
-        $controller = static::$object;
-        $view = "update";
-        $pagetitle = "Créer une commande";
-        $action = "created";
+        if ($_SESSION['admin'] == 1) {
+            $tab_prod = ModelProduit::selectAll();
+            $tab_utilisateur = ModelUtilisateur::selectAll();
+            $controller = static::$object;
+            $view = "update";
+            $pagetitle = "Créer une commande";
+            $action = "created";
         require File::build_path(array("View","view.php"));
+        } else {
+            $controller = "utilisateur";
+            $view = "connect";
+            $pagetitle = "Connectez vous pour accéder à cette fonctionnalité admin";
+            $list = File::build_path(array("View","view.php"));
+            require $list;
+        }
+        
     }
 
     public static function created(){
@@ -178,18 +206,27 @@ class ControllerCommande {
 
     }
     public static function update(){
-        $tab_prod = ModelProduit::selectAll();
+        if ($_SESSION['admin'] == 1) {
+            $tab_prod = ModelProduit::selectAll();
 
-        $controller = static::$object;
-        $idCommande = $_GET['idCommande'];
-        $idUtilisateur = $_GET['idUtilisateur'];
+            $controller = static::$object;
+            $idCommande = $_GET['idCommande'];
+            $idUtilisateur = $_GET['idUtilisateur'];
 
-        $tab_produitChecked = ModelProduit::getAllProduits($idCommande);
-        $view = "update";
-        $pagetitle = "Mettre à jour une commande";
-        $action = "updated";
-        $upd = File::build_path(array("View","view.php"));
-        require $upd;
+            $tab_produitChecked = ModelProduit::getAllProduits($idCommande);
+            $view = "update";
+            $pagetitle = "Mettre à jour une commande";
+            $action = "updated";
+            $upd = File::build_path(array("View","view.php"));
+            require $upd;
+        } else {
+            $controller = "utilisateur";
+            $view = "connect";
+            $pagetitle = "Connectez vous pour accéder à cette fonctionnalité admin";
+            $list = File::build_path(array("View","view.php"));
+            require $list;
+        }
+        
     }
 
     public static function updated(){
